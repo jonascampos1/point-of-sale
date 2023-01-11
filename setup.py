0b92e0a358ecdb172da8e88244b4b9acc46f3153
jonascampos1 <jonascampos1@hotmail.com>
2023-01-10 22:11:02 -0600

    Adding flask migrate support

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from controllers.auth import controller_endpoints
-from models.user import conn
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://jonas:password@localhost:5432/jonas'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db, directory='config/db/postgres/migrations')
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+
 controller_endpoints(app)
 
 
